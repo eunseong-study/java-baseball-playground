4131b2a6d2ade064430d1bb24dec23c1ee7f722f
dmstjd1024 <dmstjd1024@naver.com>
2022-05-27 17:59:57 +0900

    Set Collection에 대한 학습 테스트 (요구사항 2까지)

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,46 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class SetTest {
+
+	private Set<Integer> numbers;
+	
+	@BeforeEach
+	void setUp() {
+		numbers = new HashSet<>();
+		numbers.add(1);
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+	}
+	
+	/**
+	 * Set의 size() 메소드를 활용해 Set의 크기를 확인하는 학습테스트를 구현한다.
+	 */
+	@Test
+	void size() {
+		assertThat(numbers.size()).isEqualTo(3);
+	}
+	
+	/*
+	 * Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현하려한다.
+	 * 구현하고 보니 다음과 같이 중복 코드가 계속해서 발생한다.
+	 * JUnit의 ParameterizedTest를 활용해 중복 코드를 제거해 본다. 
+	 */
+	@ParameterizedTest
+	@ValueSource(ints = {1, 2, 3})
+	void isBlank_ShouldReturnTrueForNullOrBlank(int input) {
+		assertTrue(numbers.contains(input));
+	}
+
+}
